296afed910550ba726e96ec58007f97d9b2fb271
JeAlLi <jeffersonliwag1@gmail.com>
2026-09-17 08:30:29 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,11 +2,7 @@
 session_start();
 require_once 'db.php';
 
-// Ensure the user is an authenticated admin
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: index.php");
-    exit;
-}
+
 
 $current_page = 'dashboard';
 
